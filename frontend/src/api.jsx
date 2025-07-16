bbf36f36f84ea9a954ab72b6595a6639b03d3d6f
Shubham Kumar <sk6930037@gmail.com>
2025-07-16 08:47:21 +0530

    modify 4

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
-const BASE_URL =
-  'http://localhost:5000/api/coins' || 'https://my-backend-api.onrender.com/';
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const getCoins = async () => {
-  const { data } = await axios.get(BASE_URL);
+  const { data } = await axios.get(`${BASE_URL}/coins`);
   return data;
 };
