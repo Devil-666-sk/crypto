7a1c2fae388df186bcb788598afb4240bd86803f
Shubham Kumar <sk6930037@gmail.com>
2025-07-16 08:31:33 +0530

    proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // your backend URL
+        target: 'http://localhost:5000' || 'https://crypto-132j.onrender.com/',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''), // optional: removes /api prefix
