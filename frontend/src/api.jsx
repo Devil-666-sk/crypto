118c8295307a9e700a393e2fbeca362a449ecb26
Shubham Kumar <sk6930037@gmail.com>
2025-07-16 08:39:39 +0530

    modify 3

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 const BASE_URL =
-  'http://localhost:5000/api/coins' ||
-  'https://my-backend-api.onrender.com/api/coins';
+  'http://localhost:5000/api/coins' || 'https://my-backend-api.onrender.com/';
 
 export const getCoins = async () => {
   const { data } = await axios.get(BASE_URL);
